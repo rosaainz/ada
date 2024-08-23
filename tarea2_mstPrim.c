8bd2a0d3a36069acc6de6bf14d28e4cc130c9976
rosaainz <al2173040136@azc.uam.mx>
2024-08-22 21:30:29 -0600

    posibles casos

--- tarea2_mstPrim.c
+++ tarea2_mstPrim.c
@@ -2,21 +2,28 @@
 #include<limits.h>
 
 void prim(int m, int matrizAdy[m][m]){
-  int mst[m]; //arbol generador de costo minimo
-  int pesoMinimo[m];
-  int vertices[m];
+  if(m <= 1){
+    printf("0\n");
+    return;
+  }
 
+  int mst[m]; //arbol generador de costo minimo
+  int pesoMinimo[m]; //peso minimo para cada vertice
+  int vertices[m]; //conjunto de vertices incluidos en el mst
+
+  // inicializar arrays
   for(int i=0; i<m; i++){
     pesoMinimo[i] = INT_MAX;
     vertices[i] = 0;
   }
 
-  pesoMinimo[0] = 0; //inciar desde el vertice 0
+  pesoMinimo[0] = 0; //iniciar desde el vertice 0
   mst[0] = -1; //primer nodo es la raiz del mst
 
-  for(int cuenta=0; cuenta<m; cuenta++){
+  for(int cuenta=0; cuenta < m; cuenta++){
     int min= INT_MAX, indice_min;
 
+    // encontrar el vertice con el menor peso que no esta en el mst
     for(int v = 0; v < m; v++){
       if(vertices[v] == 0 && pesoMinimo[v] < min){
         min = pesoMinimo[v];
@@ -24,9 +31,11 @@ void prim(int m, int matrizAdy[m][m]){
       }
     }
 
+    // verificar si se encontro un vertice valido
     int u = indice_min;
-    vertices[u] = 1;
+    vertices[u] = 1; // incluir el vertice en el mst
 
+    // actualizar el peso y el padre de los vertices adyacentes
     for(int v = 0; v < m; v++){
       if(matrizAdy[u][v] && vertices[v] == 0 && matrizAdy[u][v] < pesoMinimo[v]){
         mst[v] = u;
@@ -35,16 +44,24 @@ void prim(int m, int matrizAdy[m][m]){
     }
   }
 
+  // calcular el costo total del mst
   int pesoMinimoTotal=0;
   for(int i=1; i < m; i++){
     pesoMinimoTotal += matrizAdy[i][mst[i]];
   }
-  printf("%d", pesoMinimoTotal);
+  printf("%d\n", pesoMinimoTotal);
 }
 
 int main(){
   int m;
   scanf("%d",&m);
+
+  if(m==1){
+    int u, v, peso;
+    scanf("%d %d %d", &u, &v, &peso);
+    printf("%d\n", peso);
+    return 0;
+  }
 
   int matrizAdy[m][m];
   for(int i=0; i<m; i++){
