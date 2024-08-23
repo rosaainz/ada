4645429a78066a3f615f94df39e0d9240d7fcf54
rosaainz <al2173040136@azc.uam.mx>
2024-08-22 19:59:06 -0600

    declarar funcion prim

--- tarea2_mstPrim.c
+++ tarea2_mstPrim.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
+#include<limits.h>
+
+void prim(int m, int matrizAdy[m][m]){
+  int mst[m]; //arbol generador de costo minimo
+  int pesoMinimo[m];
+  int vertices[m];
+
+  for(int i=0; i<m; i++){
+    pesoMinimo[i] = INT_MAX;
+    vertices[i] = 0;
+  }
+
+  pesoMinimo[0] = 0; //inciar desde el vertice 0
+  mst[0] = -1; //primer nodo es la raiz del mst
+
+}
 
 int main(){
-  int m, costo_total;
+  int m;
   scanf("%d",&m);
 
   int matrizAdy[m][m];
@@ -21,6 +37,7 @@ int main(){
     matrizAdy[v][u] = peso; //peso de v -> u
   }
 
+  prim(m, matrizAdy);
   //imprimir matriz
   for(int i=0; i<m; i++){
     for(int j=0; j<m; j++){
