346c53e48cbf0f7d9a9c3ad1c9f3d29c45d44795
rosaainz <al2173040136@azc.uam.mx>
2024-08-23 00:44:21 -0600

    metodo monedas recursivo

--- cambioMonedas.c
+++ cambioMonedas.c
@@ -1,17 +1,37 @@
 #include<stdio.h>
+#include<limits.h>
+
+int monedas(int C, int n, int denMonedas[]){
+  if(C == 0){
+    return 0;
+  }
+  if(n <= 0){
+    return INT_MAX;
+  }
+  if(denMonedas[n-1] > C){
+    return monedas(C, n-1, denMonedas);
+  }else{
+    int tomarMoneda = monedas(C - denMonedas[n-1], n, denMonedas);
+    int noTomarMoneda = monedas(C, n-1, denMonedas);
+
+    if(tomarMoneda != INT_MAX){
+      tomarMoneda += 1;
+    }
+    return (tomarMoneda < noTomarMoneda) ? tomarMoneda : noTomarMoneda;
+  }
+}
 
 int main(){
   int n, C;
 
   scanf("%d %d", &n, &C);
-  int den[n];
+  int denMonedas[n];
 
   for(int i=0; i<n; i++){
-    scanf("%d", &den[i]);
+    scanf("%d", &denMonedas[i]);
   }
 
-  for(int i=0; i<n; i++){
-    printf("%d\n",den[i]);
-  }
+  int res = monedas(C, n, denMonedas);
+  printf("%d", res);
 
 }
