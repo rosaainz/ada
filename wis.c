c9b887540e962527c05c41c055c4ee051b09b39b
rosaainz <al2173040136@azc.uam.mx>
2024-08-29 01:03:39 -0600

    Primer solucion, crear funcion wis...esta parcialmente incorrecta

--- wis.c
+++ wis.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void wis(int n, int v[]){
+  int *W = (int *)malloc((n-1) * sizeof(int));
+  int *solucion = (int *)malloc(n *  sizeof(int));
+
+  W[0] = 0;
+  W[1] = v[0];
+
+  for(int i=0; i<=n; i++){
+    W[i] = (v[i-1] + W[i-2] > W[i-1]) ? v[i-1] + W[i-2] : W[i-1];
+  }
+
+  int index = 0;
+  for(int i=n; i >= 1; i--){
+    if(i == 1 || W[i-1] < (v[i-1] + W[i-2])){
+      solucion[index++] = i;
+      i--;
+    }
+  }
+  printf("%d ", W[n]);
+  for(int i = index-1; i>=0; i--){
+    printf("%d ", solucion[i]);
+  }
+
+}
+
 
 int main(){
   int n;
@@ -11,10 +36,7 @@ int main(){
     scanf("%d", &pesos_vertices[i]);
   }
 
-  for(int i=0; i<n; i++){
-    printf("%d", pesos_vertices[i]);
-  }
-
+  wis(n, pesos_vertices);
   free(pesos_vertices);
 
   return 0;
