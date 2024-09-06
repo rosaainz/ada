501a31526029005343eb5c74c0d770e2be87285b
rosaainz <al2173040136@azc.uam.mx>
2024-09-05 18:50:44 -0600

    Recibir k consultas (origen, destino)

--- floyd.c
+++ floyd.c
@@ -25,18 +25,29 @@ int main(){
   scanf("%d %d %d", &n, &m, &k);
 
   int grafo[MAX_VERTICES][MAX_VERTICES];
+  int D[MAX_VERTICES][MAX_VERTICES];
+  int C[MAX_VERTICES][MAX_VERTICES];
+
   for(int i=0; i < MAX_VERTICES; i++){
     for(int j=0; j < MAX_VERTICES; j++){
       grafo[i][j] = (i == j) ? 0 : INF;
     }
   }
 
+  //llenar matriz de adyacencia
   for(int i=0; i < m; i++){
     int u, v, peso;
     scanf("%d %d %d", &u, &v, &peso);
     grafo[u][v] = peso;
     grafo[v][u] = peso;
   }
+
+  //k consultas
+  for(int i=0; i < k; i++){
+    int o, d; //origen y destino
+    scanf("%d %d", &o, &d);
+  }
+
 
 
   imprimirMatrizAd(grafo, m);
