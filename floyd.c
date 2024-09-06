865ffeacf23652d1521ddc51ddd8b1fb0e0d710c
rosaainz <al2173040136@azc.uam.mx>
2024-09-05 18:38:01 -0600

    Implementacion algoritmo Floyd, crear matriz de adyacencia e imprimirla

---
+++ floyd.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<limits.h>
+
+#define MAX_VERTICES 100
+#define INF INT_MAX
+
+void imprimirMatrizAd(int grafo[MAX_VERTICES][MAX_VERTICES], int m){
+  for(int i=0; i < m; i++){
+    for(int j=0; j < m; j++){
+      if(grafo[i][j] == INF){
+        printf("INF ");
+      }else{
+        printf("%d ", grafo[i][j]);
+      }
+    }
+    printf("\n");
+  }
+}
+
+int main(){
+  int n; // número de vertices de la grafica
+  int m; // número tuplas
+  int k; // consultas (origen, destino)
+
+  scanf("%d %d %d", &n, &m, &k);
+
+  int grafo[MAX_VERTICES][MAX_VERTICES];
+  for(int i=0; i < MAX_VERTICES; i++){
+    for(int j=0; j < MAX_VERTICES; j++){
+      grafo[i][j] = (i == j) ? 0 : INF;
+    }
+  }
+
+  for(int i=0; i < m; i++){
+    int u, v, peso;
+    scanf("%d %d %d", &u, &v, &peso);
+    grafo[u][v] = peso;
+    grafo[v][u] = peso;
+  }
+
+
+  imprimirMatrizAd(grafo, m);
+
+
+
+
+
+
+}
