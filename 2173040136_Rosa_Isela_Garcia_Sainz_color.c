f88a4570fb82108c07a6b3d40bcc2d409f43cb10
rosaainz <al2173040136@azc.uam.mx>
2024-09-25 18:52:06 -0600

    Graph Coloring, crear grafo y lista de adyacencia

--- 2173040136_Rosa_Isela_Garcia_Sainz_color.c
+++ 2173040136_Rosa_Isela_Garcia_Sainz_color.c
@@ -1,20 +1,67 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void leer_archivo(const char *entrada){
+#define MAX_VERTICES 100
+
+//lista de adyacencia
+typedef struct Nodo{
+  int vertice;
+  struct Nodo* siguiente;
+} Nodo;
+
+//grafo
+typedef struct Grafo{
+  Nodo* listas[MAX_VERTICES];
+  int numVertices;
+} Grafo;
+
+void addArista( Grafo* grafo, int o, int dest ){
+  Nodo* nuevoNodo = malloc(sizeof(Nodo));
+  nuevoNodo->vertice = dest;
+  nuevoNodo->siguiente = grafo->listas[o];
+  grafo->listas[o] = nuevoNodo;
+
+  nuevoNodo = malloc(sizeof(Nodo));
+  nuevoNodo->vertice = o;
+  nuevoNodo->siguiente = grafo->listas[dest];
+  grafo->listas[dest] = nuevoNodo;
+}
+
+Grafo* crearGrafo( int vertices ){
+  Grafo* grafo = malloc(sizeof(Grafo));
+  grafo->numVertices = vertices;
+ for( int i=0; i < vertices; i++ ){
+   grafo->listas[i] = NULL;
+ }
+ return grafo;
+}
+
+void leer_archivo( const char *entrada, Grafo *grafo ){
   FILE *archivo = fopen(entrada, "r");
   if( archivo == NULL ){
     printf("archivo null");
     return;
   }
 
-  char linea[256];
-  while( fgets(linea, sizeof(linea), archivo) ){
-    printf("%s", linea);
+  int o, dest;
+  while( fscanf(archivo, "%d %d", &o, &dest) == 2 ){
+    addArista(grafo, o, dest);
   }
   fclose(archivo);
 }
 
+
+void imprimirGrafo( Grafo* grafo ){
+  for( int i=0; i<grafo->numVertices; i++ ){
+    Nodo* temp = grafo->listas[i];
+    printf("Vertice: %d", i);
+    while( temp != NULL ){
+      printf(" -> %d", temp->vertice);
+      temp = temp->siguiente;
+    }
+    printf("\n");
+  }
+}
 
 int main(int argc, char *argv[]){
 
@@ -23,8 +70,23 @@ int main(int argc, char *argv[]){
     return 1;
   }
 
-  const char *entrada = argv[1];
-  leer_archivo(entrada);
+  Grafo* grafo = crearGrafo(MAX_VERTICES);
 
+  const char *entrada = argv[1];
+  leer_archivo(entrada, grafo);
+
+  imprimirGrafo(grafo);
+
+  //liberar memoria
+  for( int i=0; i<grafo->numVertices; i++ ){
+    Nodo* temp;
+    while( grafo->listas[i] != NULL ){
+      temp = grafo->listas[i];
+      grafo->listas[i] = grafo->listas[i]->siguiente;
+      free(temp);
+    }
+  }
+
+  free(grafo);
   return 0;
 }
